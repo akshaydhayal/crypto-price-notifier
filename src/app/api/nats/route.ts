24a64b5da179d0034d6da21b3d27c1672099e09d
akshaydhayal <akshaydhayalkd99@gmail.com>
2024-10-04 18:01:37 +0530

    change

--- src/app/api/nats/route.ts
+++ src/app/api/nats/route.ts
@@ -84,7 +84,7 @@ export async function GET() {
 
      setInterval(() => {
       writer.write(encoder.encode(`data: ping\n\n`));
-    }, 3800); // Send ping every 30 seconds
+    }, 2600); // Send ping every 30 seconds
   
 
     await service.serve();
