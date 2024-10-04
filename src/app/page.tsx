a5f6be99b0a5db72a0888632ef74705c36caed0d
akshaydhayal <akshaydhayalkd99@gmail.com>
2024-10-04 20:29:51 +0530

    change

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -92,7 +92,8 @@ export default function Home() {
   const handleSubmitAlert = async () => {
     try {
       // await axios.post("/api/mailalerts",{
-      await axios.post("http://ec2-13-61-2-185.eu-north-1.compute.amazonaws.com:3000/alerts", {
+      // await axios.post("https://ec2-13-61-2-185.eu-north-1.compute.amazonaws.com:3000/alerts", {
+      await axios.post("https://shopkart.akshaydhayal.xyz/alerts", {
         token: selectedToken?.id,
         symbol: selectedToken?.symbol.toUpperCase(),
         targetPrice: parseFloat(alertPrice),
